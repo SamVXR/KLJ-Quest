860286e0cf530415aa2d9cde790bbeb8ea0da348
SamVXR <sam@forklift-simulator.com>
2025-08-08 15:11:44 +0200

    added json loader

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("yow welcome");
+var json
+
+
+function start(){
+    fetchJson()
+}
+
+
+function fetchJson(){
+    fetch('data.json')
+        .then(res => res.json())
+        .then(data => loadInfo(data))
+}
+
+function loadInfo(data){
+    json = data
+    showInfo()
+}
+
+function showInfo(){
+    console.log(json.players)
+}
+
+
+
+
+start()
